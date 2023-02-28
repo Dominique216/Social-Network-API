8285a2d9cca75e01796c483ffd082fa2d716a0cb
Dominique Nix <raquel_0216@yahoo.com>
2023-02-27 20:26:24 -0600

    added models for users and thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,35 @@
-// thought model
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+    }
+)
+
+// retrieves the length of the user's friends
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+  const Thought = model('Thought', thoughtrSchema);
+
+  module.exports = Thought;
