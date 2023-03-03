bb3cf2c17c0f6b027c50448a657b0d5a84382d38
Dominique Nix <raquel_0216@yahoo.com>
2023-03-03 11:47:43 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://drn:Gina1601@cluster0.224mut0.mongodb.net/studentsDB';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
